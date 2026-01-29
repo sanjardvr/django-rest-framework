e7105729882a663c15a3aa3cf2ddaecc2cd821bb
sanjardvr <lazabdufattahovsanjar10@gmail.com>
2026-01-29 16:33:48 +0500

    postgres database connected

--- manage.py
+++ manage.py
@@ -2,6 +2,9 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv
+
+
 
 
 def main():
@@ -19,4 +22,5 @@ def main():
 
 
 if __name__ == '__main__':
+    load_dotenv()
     main()
