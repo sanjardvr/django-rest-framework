151a7546c4a916c476abbaead4429f295c32ae13
sanjardvr <lazabdufattahovsanjar10@gmail.com>
2026-02-01 07:51:17 +0500

    adding serializer

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.forms import model_to_dict
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from apps.product.models import Product
+from apps.product.serializers import ProductSerializer
+
+@api_view(["GET" , "POST"])
+def api_home(request, *args, **kwargs):
+    """
+    DRF API View
+    """
+    instance = Product.objects.all().order_by("?").first()
+    data = {}
+    if instance:
+        # data = model_to_dict(model_data, fields=['__all__'])
+        data = ProductSerializer(instance).data
+        return Response(data)
--- src/settings.py
+++ src/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third party
+    'rest_framework',
+    # Internal
+    "apps.api",
+    "apps.product"
 ]
 
 MIDDLEWARE = [
